fba513a8bfcab061e5c218c7d8add1380e8b7e3c
Marco <marco.riggirello@libero.it>
2022-01-10 16:02:38 +0100

    Navigation in wez

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -6,5 +6,9 @@ return {
   color_scheme = "colors-wez",
   color_scheme_dirs = {HOME.."/.cache/wal/"},
   hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    -- Default ones do not work in italian keyboard.
+    {key="s", mods="CTRL|ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    {key="s", mods="CTRL|ALT|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  }
 }
-
